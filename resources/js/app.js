1bfb2c499f681e92d7e3c26b73d6ea9def888227
ryotaro-boost <ryotaro.t.0727@gmail.com>
2020-04-07 22:56:32 +0900

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+
+import './bootstrap'
+import Vue from 'vue'
+import ArticleLike from './components/ArticleLike'
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    ArticleLike,
+  }
+})
