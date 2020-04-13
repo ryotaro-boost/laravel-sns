de45dc37f77cb58fe82417a64beec5e4ff24393b
ryotaro-boost <ryotaro.t.0727@gmail.com>
2020-04-13 20:33:20 +0900

    第8章まで

--- app/Article.php
+++ app/Article.php
@@ -33,4 +33,8 @@ class Article extends Model
     {
         return $this->likes->count();
     }
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
 }
